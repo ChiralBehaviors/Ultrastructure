6e2d595229bed77455f0e4b221803afd3deaaee6
Hellblazer <hellblazer@me.com>
2013-06-06 20:28:23 -0700

    Fix version skew with open jpa.  Use requesting resource, not assign to in protocol attribute authorization.

--- model/src/main/java/com/hellblazer/CoRE/event/ProtocolAttributeAuthorization.java
+++ model/src/main/java/com/hellblazer/CoRE/event/ProtocolAttributeAuthorization.java
@@ -22,6 +22,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -31,61 +33,111 @@ import com.hellblazer.CoRE.location.Location;
 import com.hellblazer.CoRE.network.Relationship;
 import com.hellblazer.CoRE.product.Product;
 import com.hellblazer.CoRE.resource.Resource;
+import static com.hellblazer.CoRE.network.Networked.*;
+import static com.hellblazer.CoRE.event.ProtocolAttributeAuthorization.*;
 
 /**
  * @author hhildebrand
  * 
  */
+@NamedQueries({
+               @NamedQuery(name = FIND_CLASSIFIED_ATTRIBUTE_VALUES, query = "SELECT "
+                                                                            + "  attrValue "
+                                                                            + "FROM "
+                                                                            + "       ProtocolAttribute attrValue, "
+                                                                            + "       ProtocolAttributeAuthorization auth, "
+                                                                            + "       ProductNetwork network, "
+                                                                            + "WHERE "
+                                                                            + "        auth.authorizedAttribute = attrValue.attribute AND "
+                                                                            + "        network.relationship = auth.service_classification AND "
+                                                                            + "        network.parent = auth.service AND"
+                                                                            + "        network.relationship = auth.product_classification AND "
+                                                                            + "        network.parent = auth.product AND"
+                                                                            + "        attrValue.protocol = :ruleform AND "
+                                                                            + "        auth.classification = :classification AND "
+                                                                            + "        auth.classifier = :classifier "),
+               @NamedQuery(name = FIND_GROUPED_ATTRIBUTE_VALUES, query = "select attr from ProtocolAttributeAuthorization attr where "
+                                                                         + "attr.product = :ruleform "
+                                                                         + "AND attr.id IN ("
+                                                                         + "select ea.authorizedAttribute from ProductAttributeAuthorization ea "
+                                                                         + "WHERE ea.groupingResource = :resource)"),
+               @NamedQuery(name = FIND_CLASSIFIED_ATTRIBUTE_AUTHORIZATIONS, query = "select ea from ProtocolAttributeAuthorization ea "
+                                                                                    + "WHERE ea.classification = :classification "
+                                                                                    + "AND ea.classifier = :classifier"),
+               @NamedQuery(name = FIND_CLASSIFIED_ATTRIBUTE_AUTHORIZATIONS_FOR_ATTRIBUTE, query = "select ea from ProtocolAttributeAuthorization ea "
+                                                                                                  + "WHERE ea.classification = :classification "
+                                                                                                  + "AND ea.classifier = :classifier AND ea.authorizedAttribute = :attribute"),
+               @NamedQuery(name = FIND_GROUPED_ATTRIBUTE_AUTHORIZATIONS, query = "select ea from ProductAttributeAuthorization ea "
+                                                                                 + "WHERE ea.groupingResource = :groupingResource"),
+               @NamedQuery(name = FIND_GROUPED_ATTRIBUTE_AUTHORIZATIONS_FOR_ATTRIBUTE, query = "select ea from ProtocolAttributeAuthorization ea "
+                                                                                               + "WHERE ea.groupingResource = :groupingResource AND ea.authorizedAttribute = :attribute"),
+               @NamedQuery(name = FIND_ATTRIBUTE_AUTHORIZATIONS, query = "select ea from ProtocolAttributeAuthorization ea "
+                                                                         + "WHERE ea.classification = :classification "
+                                                                         + "AND ea.classifier = :classifier "
+                                                                         + "AND ea.groupingResource = :groupingResource") })
 @javax.persistence.Entity
 @Table(name = "protocol_attribute_authorization", schema = "ruleform")
 @SequenceGenerator(schema = "ruleform", name = "protocol_attribute_authorization_id_seq", sequenceName = "protocol_attribute_authorization_id_seq")
 public class ProtocolAttributeAuthorization extends AttributeAuthorization {
+    public static final String FIND_ATTRIBUTE_AUTHORIZATIONS                          = "protocolAttributeAuthorization.findAttributeAuthorizations";
+    public static final String FIND_CLASSIFIED_ATTRIBUTE_AUTHORIZATIONS               = "protocolAttributeAuthorization"
+                                                                                        + FIND_CLASSIFIED_ATTRIBUTE_AUTHORIZATIONS_SUFFIX;
+    public static final String FIND_CLASSIFIED_ATTRIBUTE_AUTHORIZATIONS_FOR_ATTRIBUTE = "protocolAttributeAuthorization"
+                                                                                        + FIND_CLASSIFIED_ATTRIBUTE_AUTHORIZATIONS_FOR_ATTRIBUTE_SUFFIX;
+    public static final String FIND_CLASSIFIED_ATTRIBUTE_VALUES                       = "protocolAttributeAuthorization"
+                                                                                        + FIND_CLASSIFIED_ATTRIBUTE_VALUES_SUFFIX;
+    public static final String FIND_GROUPED_ATTRIBUTE_AUTHORIZATIONS                  = "protocolAttributeAuthorization"
+                                                                                        + FIND_GROUPED_ATTRIBUTE_ATHORIZATIONS_SUFFIX;
+    public static final String FIND_GROUPED_ATTRIBUTE_AUTHORIZATIONS_FOR_ATTRIBUTE    = "protocolAttributeAuthorization"
+                                                                                        + FIND_GROUPED_ATTRIBUTE_ATHORIZATIONS_FOR_ATTRIBUTE_SUFFIX;
+    public static final String FIND_GROUPED_ATTRIBUTE_VALUES                          = "protocolAttributeAuthorization"
+                                                                                        + FIND_GROUPED_ATTRIBUTE_VALUES_SUFFIX;
 
-    private static final long serialVersionUID = 1L;
-
-    @ManyToOne
-    @JoinColumn(name = "assign_to")
-    private Resource          assignTo;
-
-    @ManyToOne
-    @JoinColumn(name = "assign_to_classification")
-    private Relationship      assignToClassification;
+    private static final long  serialVersionUID                                       = 1L;
 
     @ManyToOne
     @JoinColumn(name = "deliver_from")
-    private Location          deliverFrom;
+    private Location           deliverFrom;
 
     @ManyToOne
     @JoinColumn(name = "deliver_from_classification")
-    private Relationship      deliverFromClassification;
+    private Relationship       deliverFromClassification;
 
     @ManyToOne
     @JoinColumn(name = "deliver_to")
-    private Location          deliverTo;
+    private Location           deliverTo;
 
     @ManyToOne
     @JoinColumn(name = "deliver_to_classification")
-    private Relationship      deliverToClassification;
+    private Relationship       deliverToClassification;
 
     @Id
     @GeneratedValue(generator = "protocol_attribute_authorization_id_seq", strategy = GenerationType.SEQUENCE)
-    private Long              id;
+    private Long               id;
 
     @ManyToOne
     @JoinColumn(name = "product_classification")
-    private Relationship      procuctClassification;
+    private Relationship       procuctClassification;
 
     @ManyToOne
     @JoinColumn(name = "product")
-    private Product           product;
+    private Product            product;
+
+    @ManyToOne
+    @JoinColumn(name = "requester")
+    private Resource           requester;
+
+    @ManyToOne
+    @JoinColumn(name = "requester_classification")
+    private Relationship       requesterClassification;
 
     @ManyToOne
     @JoinColumn(name = "service")
-    private Product           service;
+    private Product            service;
 
     @ManyToOne
     @JoinColumn(name = "service_classification")
-    private Relationship      serviceClassification;
+    private Relationship       serviceClassification;
 
     /**
      * 
@@ -124,14 +176,7 @@ public class ProtocolAttributeAuthorization extends AttributeAuthorization {
     public ProtocolAttributeAuthorization(Resource updatedBy) {
         super(updatedBy);
     }
-
-    public Resource getAssignTo() {
-        return assignTo;
-    }
-
-    public Relationship getAssignToClassification() {
-        return assignToClassification;
-    }
+ 
 
     public Location getDeliverFrom() {
         return deliverFrom;
@@ -165,20 +210,21 @@ public class ProtocolAttributeAuthorization extends AttributeAuthorization {
         return product;
     }
 
+    public Resource getRequester() {
+        return requester;
+    }
+
+    public Relationship getRequesterClassification() {
+        return requesterClassification;
+    }
+ 
+
     public Product getService() {
         return service;
     }
 
     public Relationship getServiceClassification() {
         return serviceClassification;
-    }
-
-    public void setAssignTo(Resource assignTo) {
-        this.assignTo = assignTo;
-    }
-
-    public void setAssignToClassification(Relationship assignToClassification) {
-        this.assignToClassification = assignToClassification;
     }
 
     public void setDeliverFrom(Location deliverFrom) {
@@ -211,6 +257,14 @@ public class ProtocolAttributeAuthorization extends AttributeAuthorization {
 
     public void setProduct(Product product) {
         this.product = product;
+    }
+
+    public void setRequester(Resource requester) {
+        this.requester = requester;
+    }
+
+    public void setRequesterClassification(Relationship requesterClassification) {
+        this.requesterClassification = requesterClassification;
     }
 
     public void setService(Product service) {

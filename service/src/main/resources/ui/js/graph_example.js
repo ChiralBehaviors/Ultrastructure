7d42dfbfbd8cf00f8fc07102c3d4388839a894fa
Hallie Parry <halloran.parry@gmail.com>
2013-09-04 10:55:53 -0700

    Figured out the node add issue. Need to define properties when I reference them

--- service/src/main/resources/ui/js/graph_example.js
+++ service/src/main/resources/ui/js/graph_example.js
@@ -6,43 +6,16 @@ var r = 10;
 var nodes = [];
 var links = [];
 
+var counter = 800;
+
 var colors = d3.scale.category10();
-
-var force = d3.layout.force().size([ w, h ]).linkDistance([ 50 ]).charge(
-		[ -100 ]).on("tick", tick);
-
-var svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
-
-svg.append("svg:defs").append("svg:marker").attr("id", "marker").attr(
-		"viewBox", "0 -5 10 10").attr("refX", 15).attr("refY", -1.5).attr(
-		"markerWidth", 6).attr("markerHeight", 6).attr("orient", "auto")
-		.append("svg:path").attr("d", "M0,-5L10,0L0,5");
-
-var node = svg.selectAll(".node"), link = svg.selectAll(".link");
-
-function start() {
-	link = link.data(force.links(), function(d) {
-		return d.source + "-" + d.target;
-	});
-	link.enter().insert("line", ".node").attr("class", "link");
-	link.exit().remove();
-
-	node = node.data(force.nodes(), function(d) {
-		return d.id;
-	});
-	node.enter().append("circle").attr("class", function(d) {
-		return "node " + d.id;
-	}).attr("r", r).call(force.drag);
-	node.exit().remove();
-
-	force.start();
-}
-
 function tick() {
 	node.attr("cx", function(d) {
-		return d.x = Math.max(r, Math.min(w - r, d.x));
+		d.x = Math.max(r, Math.min(w - r, d.x));
+        return d.x;
 	}).attr("cy", function(d) {
-		return d.y = Math.max(r, Math.min(h - r, d.y));
+		d.y = Math.max(r, Math.min(h - r, d.y));
+        return d.y;
 	});
 
 	link.attr("x1", function(d) {
@@ -55,18 +28,80 @@ function tick() {
 		return d.target.y;
 	});
 }
+
+var force = d3.layout.force()
+                .size([ w, h ])
+                .linkDistance([ 50 ])
+                .charge([ -100 ])
+                .on("tick", tick);
+
+//create the SVG element
+var svg = d3.select("body")
+			.append("svg")
+			.attr("width", w)
+			.attr("height", h)
+			.on("dblclick", function() {
+				var n = {name: "new node", id: ++counter};
+				nodes.push(n);
+				start();
+			});
+
+//append the marker definition to the SVG element
+//this is necessary for the arrow heads on the directed edges
+svg.append("svg:defs")
+	.append("svg:marker")
+	.attr("id", "marker")
+	.attr("viewBox", "0 -5 10 10")
+	.attr("refX", 15)
+	.attr("refY", -1.5)
+	.attr("markerWidth", 6)
+	.attr("markerHeight", 6)
+	.attr("orient", "auto")
+	.append("svg:path")
+	.attr("d", "M0,-5L10,0L0,5");
+
+var node = svg.selectAll(".node"), 
+	link = svg.selectAll(".link");
+
+function start() {
+//	console.log("links: " + force.links());
+//	link = link.data(force.links(), function(d) {
+//		return d.source + "-" + d.target;
+//	});
+//	console.log("link: " + link);
+//	link.enter()
+//		.insert("line", ".node")
+//		.attr("class", "link");
+//	link.exit()
+//		.remove();
+
+	node = node.data(force.nodes(), function(d) {
+		return d.id;
+	});
+	node.enter()
+		.append("circle")
+		.attr("class", function(d) {
+			return "node " + d.id;
+		})
+		.attr("r", r).call(force.drag);
+	node.exit().remove();
+
+	force.start();
+}
+
+
 $.getJSON("http://localhost:8080/v1/services/data/collection/2?rel=15",
 		function(dataset) {
 			$.each(dataset.nodes, function(d, i) {
 				nodes.push(i);
 			});
-			$.each(dataset.edges, function(d, i) {
-				links.push(i);
-			});
+//			$.each(dataset.edges, function(d, i) {
+//				links.push(i);
+//			});
 			console.log(nodes);
-			console.log(links);
+			//console.log(links);
 			force.nodes(nodes);
-			force.links(links);
+			//force.links(links);
 			start();
 
 		});

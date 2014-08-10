904748a253bb94120838a63a41d0f118fe3a7d2b
Hellblazer <hellblazer@me.com>
2014-08-10 14:16:44 -0700

    add workspace authorization

---
+++ workspace-generator/src/main/java/com/chiralbehaviors/CoRE/generators/WorkspaceGenerator.java
@@ -0,0 +1,197 @@
+/** 
+ * (C) Copyright 2012 Chiral Behaviors, LLC. All Rights Reserved
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+
+package com.chiralbehaviors.CoRE.generators;
+
+import static java.lang.Character.isUpperCase;
+import static java.lang.Character.toLowerCase;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.StringWriter;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.stringtemplate.v4.ST;
+import org.stringtemplate.v4.STGroup;
+import org.stringtemplate.v4.STGroupFile;
+
+/**
+ * @author hhildebrand
+ *
+ * 
+ * @goal generate
+ * 
+ * @phase generate-sources
+ */
+public class WorkspaceGenerator extends AbstractMojo {
+    private static final String   DEFAULT         = "target/generated-sources/workspace";
+    private static final String   PREFIX          = "com.chiralbehaviors.CoRE.";
+    private static final String   SOURCE_FILE     = "com/chiralbehaviors/CoRE/workspace/WorkspaceAuthorization.java";
+    private static final String[] imports         = { "agency.Agency",
+            "agency.AgencyAttribute", "agency.AgencyAttributeAuthorization",
+            "agency.AgencyLocation", "agency.AgencyLocationAttribute",
+            "agency.AgencyNetwork", "agency.AgencyNetworkAttribute",
+            "agency.AgencyNetworkAuthorization", "agency.AgencyProduct",
+            "agency.access.AgencyLocationAccessAuthorization",
+            "agency.access.AgencyProductAccessAuthorization",
+            "attribute.Attribute",
+            "attribute.AttributeMetaAttribute",
+            "attribute.AttributeMetaAttributeAuthorization",
+            "attribute.AttributeNetwork",
+            "attribute.AttributeNetworkAttribute",
+            "attribute.unit.Unit",
+            "attribute.unit.UnitAttribute",
+            "attribute.unit.UnitAttributeAuthorization",
+            "attribute.unit.UnitNetwork",
+            "attribute.unit.UnitNetworkAttribute",
+            "attribute.unit.UnitValue",
+            "location.Location",
+            "location.LocationAttribute",
+            "location.LocationAttributeAuthorization",
+            "location.LocationNetwork",
+            "location.LocationNetworkAttribute",
+            "location.LocationNetworkAuthorization",
+            "product.Product",
+            "product.ProductAttribute",
+            "product.ProductAttributeAuthorization",
+            "product.ProductNetwork",
+            "product.ProductNetworkAttribute",
+            "product.ProductLocation",
+            "product.ProductLocationAttribute",
+            "time.Interval",
+            "time.IntervalAttribute",
+            "time.IntervalAttributeAuthorization",
+            "time.IntervalNetwork",
+            "time.IntervalNetworkAttribute",
+            "event.status.StatusCode",
+            "event.status.StatusCodeAttribute",
+            "event.status.StatusCodeAttributeAuthorization",
+            "event.status.StatusCodeNetwork",
+            "event.status.StatusCodeNetworkAttribute",
+            "event.status.StatusCodeSequencing",
+            "event.Job",
+            "event.JobChronology",
+            "event.MetaProtocol",
+            "event.ProductChildSequencingAuthorization",
+            "event.ProductParentSequencingAuthorization",
+            "event.ProductSiblingSequencingAuthorization",
+            "event.Protocol"};
+
+    /**
+     * Target generation directory
+     * 
+     * @parameter
+     */
+    private File                  outputDirectory = new File(DEFAULT);
+
+    /* (non-Javadoc)
+     * @see org.apache.maven.plugin.Mojo#execute()
+     */
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        List<String> types = getTypes();
+        List<String> variables = getVariables(types);
+        STGroup group = new STGroupFile("templates/workspace.stg");
+        ST workspace = group.getInstanceOf("generate");
+        workspace.add("imports", getImports());
+        workspace.add("types", types);
+        workspace.add("variables", variables);
+        workspace.add("columns", getColumns(variables));
+
+        File sourceFile = new File(outputDirectory, SOURCE_FILE);
+        try {
+            Files.deleteIfExists(sourceFile.toPath());
+        } catch (FileNotFoundException e) {
+            throw new MojoExecutionException(
+                                             String.format("Cannot find file for create %s",
+                                                           sourceFile.getAbsolutePath(),
+                                                           e));
+        } catch (IOException e) {
+
+            throw new MojoExecutionException(
+                                             String.format("Error creating file %s\nCause: %s",
+                                                           sourceFile.getAbsolutePath(),
+                                                           e.getMessage()));
+        }
+        sourceFile.getParentFile().mkdirs();
+        try (OutputStream os = new FileOutputStream(
+                                                    Files.createFile(sourceFile.toPath()).toFile())) {
+            os.write(workspace.render().getBytes());
+        } catch (IOException e) {
+            throw new MojoExecutionException(
+                                             String.format("Error writing file %s",
+                                                           sourceFile.getAbsolutePath(),
+                                                           e));
+        }
+    }
+
+    /**
+     * @return
+     */
+    private List<String> getImports() {
+        List<String> declarations = new ArrayList<>();
+        for (String decl : imports) {
+            declarations.add(PREFIX + decl);
+        }
+        return declarations;
+    }
+
+    /**
+     * @param variables
+     * @return
+     */
+    private List<String> getColumns(List<String> variables) {
+        List<String> columns = new ArrayList<>();
+        for (String variable : variables) {
+            StringWriter writer = new StringWriter(variable.length() + 3);
+            for (char character : variable.toCharArray()) {
+                if (isUpperCase(character)) {
+                    writer.write('_');
+                    writer.write(toLowerCase(character));
+                } else {
+                    writer.write(character);
+                }
+            }
+            columns.add(writer.toString());
+        }
+        return columns;
+    }
+
+    private List<String> getTypes() {
+        List<String> types = new ArrayList<>();
+        for (String type : imports) {
+            int pos = type.lastIndexOf(".");
+            types.add(type.substring(pos + 1));
+        }
+        return types;
+    }
+
+    private List<String> getVariables(List<String> types) {
+        List<String> variables = new ArrayList<>();
+        for (String type : types) {
+            variables.add(toLowerCase(type.charAt(0)) + type.substring(1));
+        }
+        return variables;
+    }
+}

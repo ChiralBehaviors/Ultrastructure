4bd552f977641dce03ea1fdca52a529547b6b2cc
Hallie Parry <halloran.parry@gmail.com>
2015-05-26 08:55:02 -0700

    This test doesn't pass and I don't know why

---
+++ animations/src/test/java/com/chiralbehaviors/phantasm/demo/Journey.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2015 Chiral Behaviors, LLC, all rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.chiralbehaviors.phantasm.demo;
+
+import java.util.List;
+
+import com.chiralbehaviors.CoRE.phantasm.ScopedPhantasm;
+import com.chiralbehaviors.CoRE.time.Interval;
+import com.chiralbehaviors.CoRE.annotations.Key;
+import com.chiralbehaviors.CoRE.annotations.State;
+
+/**
+ * @author hparry
+ *
+ */
+@State(workspace = "uri:http://ultrastructure.me/ontology/com.chiralbehaviors/demo/steward-workspace/v1")
+public interface Journey extends ScopedPhantasm<Interval>{
+
+    @Key(namespace = "kernel", name = "HasMember")
+    List<Step> getSteps();
+
+    @Key(namespace = "kernel", name = "HasMember")
+    void addStep(Step step);
+}

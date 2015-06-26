51b044a97efd57831dd01aae5524fb3714f5c587
Hellblazer <hellblazer@me.com>
2015-06-26 16:25:55 -0700

    implement all the existentials

--- phantasm-at-rest/src/main/java/com/chiralbehaviors/CoRE/phantasm/resource/ContextResource.java
+++ phantasm-at-rest/src/main/java/com/chiralbehaviors/CoRE/phantasm/resource/ContextResource.java
@@ -35,13 +35,23 @@ import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
 
 import com.chiralbehaviors.CoRE.ExistentialRuleform;
+import com.chiralbehaviors.CoRE.agency.Agency;
 import com.chiralbehaviors.CoRE.attribute.Attribute;
 import com.chiralbehaviors.CoRE.attribute.AttributeAuthorization;
 import com.chiralbehaviors.CoRE.attribute.AttributeValue;
+import com.chiralbehaviors.CoRE.attribute.unit.Unit;
+import com.chiralbehaviors.CoRE.job.status.StatusCode;
+import com.chiralbehaviors.CoRE.location.Location;
+import com.chiralbehaviors.CoRE.meta.AgencyModel;
 import com.chiralbehaviors.CoRE.meta.Aspect;
+import com.chiralbehaviors.CoRE.meta.LocationModel;
 import com.chiralbehaviors.CoRE.meta.NetworkedModel;
+import com.chiralbehaviors.CoRE.meta.ProductModel;
+import com.chiralbehaviors.CoRE.meta.RelationshipModel;
 import com.chiralbehaviors.CoRE.network.NetworkAuthorization;
 import com.chiralbehaviors.CoRE.product.Product;
+import com.chiralbehaviors.CoRE.relationship.Relationship;
+import com.chiralbehaviors.CoRE.time.Interval;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -72,31 +82,100 @@ public class ContextResource extends TransactionalResource {
     }
 
     @Produces({ "application/json", "text/json" })
+    @Path("agency/{classifier}/{classification}")
+    @GET
+    public JsonNode getAgencyContext(@PathParam(CLASSIFIER) String relationship,
+                                     @PathParam(CLASSIFICATION) String ruleform) {
+        AgencyModel agencyModel = readOnlyModel.getAgencyModel();
+        Aspect<Agency> aspect = getAspect(relationship, ruleform, agencyModel);
+        return buildContext(aspect, agencyModel,
+                            Agency.class.getSimpleName().toLowerCase());
+    }
+
+    @Produces({ "application/json", "text/json" })
+    @Path("attribute/{classifier}/{classification}")
+    @GET
+    public JsonNode getAttributeContext(@PathParam(CLASSIFIER) String relationship,
+                                        @PathParam(CLASSIFICATION) String ruleform) {
+        return buildContext(relationship, ruleform,
+                            readOnlyModel.getAttributeModel(),
+                            Attribute.class.getSimpleName().toLowerCase());
+    }
+
+    @Produces({ "application/json", "text/json" })
+    @Path("interval/{classifier}/{classification}")
+    @GET
+    public JsonNode getIntervalContext(@PathParam(CLASSIFIER) String relationship,
+                                       @PathParam(CLASSIFICATION) String ruleform) {
+        return buildContext(relationship, ruleform,
+                            readOnlyModel.getIntervalModel(),
+                            Interval.class.getSimpleName().toLowerCase());
+    }
+
+    @Produces({ "application/json", "text/json" })
+    @Path("location/{classifier}/{classification}")
+    @GET
+    public JsonNode getLocationContext(@PathParam(CLASSIFIER) String relationship,
+                                       @PathParam(CLASSIFICATION) String ruleform) {
+        LocationModel locationModel = readOnlyModel.getLocationModel();
+        Aspect<Location> aspect = getAspect(relationship, ruleform,
+                                            locationModel);
+        return buildContext(aspect, locationModel,
+                            Location.class.getSimpleName().toLowerCase());
+    }
+
+    @Produces({ "application/json", "text/json" })
     @Path("product/{classifier}/{classification}")
     @GET
     public JsonNode getProductContext(@PathParam(CLASSIFIER) String relationship,
                                       @PathParam(CLASSIFICATION) String ruleform) {
-        UUID classifier;
-        try {
-            classifier = UUID.fromString(relationship);
-        } catch (IllegalArgumentException e) {
-            throw new WebApplicationException(e, Response.Status.BAD_REQUEST);
-        }
+        ProductModel productModel = readOnlyModel.getProductModel();
+        Aspect<Product> aspect = getAspect(relationship, ruleform,
+                                           productModel);
+        return buildContext(aspect, productModel,
+                            Product.class.getSimpleName().toLowerCase());
+    }
+
+    @Produces({ "application/json", "text/json" })
+    @Path("relationship/{classifier}/{classification}")
+    @GET
+    public JsonNode getRelationshipContext(@PathParam(CLASSIFIER) String relationship,
+                                           @PathParam(CLASSIFICATION) String ruleform) {
+        RelationshipModel relationshipModel = readOnlyModel.getRelationshipModel();
+        Aspect<Relationship> aspect = getAspect(relationship, ruleform,
+                                                relationshipModel);
+        return buildContext(aspect, relationshipModel,
+                            Relationship.class.getSimpleName().toLowerCase());
+    }
+
+    @Produces({ "application/json", "text/json" })
+    @Path("statusCode/{classifier}/{classification}")
+    @GET
+    public JsonNode getStatusCodeContext(@PathParam(CLASSIFIER) String relationship,
+                                         @PathParam(CLASSIFICATION) String ruleform) {
+        return buildContext(relationship, ruleform,
+                            readOnlyModel.getStatusCodeModel(),
+                            StatusCode.class.getSimpleName().toLowerCase());
+    }
+
+    @Produces({ "application/json", "text/json" })
+    @Path("unit/{classifier}/{classification}")
+    @GET
+    public JsonNode getUnitContext(@PathParam(CLASSIFIER) String relationship,
+                                   @PathParam(CLASSIFICATION) String ruleform) {
+        return buildContext(relationship, ruleform,
+                            readOnlyModel.getUnitModel(),
+                            Unit.class.getSimpleName().toLowerCase());
+    }
+
+    public UUID toUuid(String ruleform) {
         UUID classification;
         try {
             classification = UUID.fromString(ruleform);
         } catch (IllegalArgumentException e) {
             throw new WebApplicationException(e, Response.Status.BAD_REQUEST);
         }
-        Aspect<Product> aspect;
-        NetworkedModel<Product, ?, ?, ?> networkedModel = readOnlyModel.getProductModel();
-        try {
-            aspect = networkedModel.getAspect(classifier, classification);
-        } catch (IllegalArgumentException e) {
-            throw new WebApplicationException(e, Response.Status.NOT_FOUND);
-        }
-
-        return buildContext(aspect, networkedModel);
+        return classification;
     }
 
     private <RuleForm extends ExistentialRuleform<RuleForm, ?>> void addAttributeTerms(ObjectNode context,
@@ -119,15 +198,34 @@ public class ContextResource extends TransactionalResource {
 
     private <RuleForm extends ExistentialRuleform<RuleForm, ?>> void addNetworkAuthTerms(ObjectNode context,
                                                                                          Aspect<RuleForm> aspect,
-                                                                                         NetworkedModel<RuleForm, ?, ?, ?> networkedModel) {
+                                                                                         NetworkedModel<RuleForm, ?, ?, ?> networkedModel,
+                                                                                         String eeType) {
         for (NetworkAuthorization<RuleForm> auth : networkedModel.getNetworkAuthorizations(aspect)) {
             Aspect<RuleForm> childAspect = new Aspect<RuleForm>(auth.getAuthorizedRelationship(),
                                                                 auth.getAuthorizedParent());
             if (auth.getName() != null) {
                 ObjectNode termDefinition = new ObjectNode(JsonNodeFactory.withExactBigDecimals(true));
-                termDefinition.put(ID,
-                                   getTypeIri(Product.class.getSimpleName().toLowerCase(),
-                                              childAspect));
+                termDefinition.put(ID, getTypeIri(eeType, childAspect));
+                termDefinition.put(TYPE, ID);
+                termDefinition.put(CLASSIFIER,
+                                   childAspect.getClassifier().getName());
+                termDefinition.put(CLASSIFICATION,
+                                   childAspect.getClassification().getName());
+                context.set(auth.getName(), termDefinition);
+            }
+        }
+    }
+
+    private <RuleForm extends ExistentialRuleform<RuleForm, ?>> void addXdomainAuthTerms(ObjectNode context,
+                                                                                         Aspect<RuleForm> aspect,
+                                                                                         NetworkedModel<RuleForm, ?, ?, ?> networkedModel,
+                                                                                         String eeType) {
+        for (NetworkAuthorization<RuleForm> auth : networkedModel.getNetworkAuthorizations(aspect)) {
+            Aspect<RuleForm> childAspect = new Aspect<RuleForm>(auth.getAuthorizedRelationship(),
+                                                                auth.getAuthorizedParent());
+            if (auth.getName() != null) {
+                ObjectNode termDefinition = new ObjectNode(JsonNodeFactory.withExactBigDecimals(true));
+                termDefinition.put(ID, getTypeIri(eeType, childAspect));
                 termDefinition.put(TYPE, ID);
                 termDefinition.put(CLASSIFIER,
                                    childAspect.getClassifier().getName());
@@ -139,30 +237,45 @@ public class ContextResource extends TransactionalResource {
     }
 
     private <RuleForm extends ExistentialRuleform<RuleForm, ?>> JsonNode buildContext(Aspect<RuleForm> aspect,
-                                                                                      NetworkedModel<RuleForm, ?, ?, ?> networkedModel) {
+                                                                                      NetworkedModel<RuleForm, ?, ?, ?> networkedModel,
+                                                                                      String eeType) {
         ObjectNode container = new ObjectNode(JsonNodeFactory.withExactBigDecimals(true));
         ObjectNode context = new ObjectNode(JsonNodeFactory.withExactBigDecimals(true));
         container.set(CONTEXT, context);
         addAttributeTerms(context, aspect, networkedModel);
-        addNetworkAuthTerms(context, aspect, networkedModel);
+        addNetworkAuthTerms(context, aspect, networkedModel, eeType);
+        addXdomainAuthTerms(context, aspect, networkedModel, eeType);
         return container;
     }
 
-    /**
-     * @param eeType
-     * @param aspect
-     * @return
-     */
+    private <RuleForm extends ExistentialRuleform<RuleForm, ?>> JsonNode buildContext(String relationship,
+                                                                                      String ruleform,
+                                                                                      NetworkedModel<RuleForm, ?, ?, ?> networkedModel,
+                                                                                      String eeType) {
+        return buildContext(getAspect(relationship, ruleform, networkedModel),
+                            networkedModel, eeType);
+
+    }
+
+    private <RuleForm extends ExistentialRuleform<RuleForm, ?>> Aspect<RuleForm> getAspect(String relationship,
+                                                                                           String ruleform,
+                                                                                           NetworkedModel<RuleForm, ?, ?, ?> networkedModel) {
+
+        UUID classifier = toUuid(relationship);
+        UUID classification = toUuid(ruleform);
+        try {
+            return networkedModel.getAspect(classifier, classification);
+        } catch (IllegalArgumentException e) {
+            throw new WebApplicationException(e, Response.Status.NOT_FOUND);
+        }
+    }
+
     private String getTypeIri(String eeType, Aspect<?> aspect) {
         UriBuilder ub = uriInfo.getBaseUriBuilder();
         URI userUri = ub.path(ContextResource.class).path(aspect.getClassifier().getId().toString()).path(aspect.getClassification().getId().toString()).build();
         return userUri.toASCIIString();
     }
 
-    /**
-     * @param authorizedAttribute
-     * @return
-     */
     private String iriFrom(Attribute authorizedAttribute) {
         AttributeValue<Attribute> iri = readOnlyModel.getAttributeModel().getAttributeValue(authorizedAttribute,
                                                                                             readOnlyModel.getKernel().getIRI());

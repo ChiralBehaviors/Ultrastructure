d93ef847776a38d597ceb956bb64639efa82ef07
Hellblazer <hellblazer@me.com>
2015-07-19 18:46:30 -0700

    more progress

--- phantasm-at-rest/src/test/resources/assets/js/controllers.js
+++ phantasm-at-rest/src/test/resources/assets/js/controllers.js
@@ -1,22 +1,56 @@
 var usBrowserControllers = angular.module('usBrowserControllers', []);
 
-usBrowserControllers.controller('FacetListCtrl', ['$scope', '$http','$routeParams', 
-  function ($scope, $http, $routeParams) {
-    $http.get('/json-ld/facet/' + $routeParams.ruleform).success(function(data) {
-      $scope.facets = data;
-    });
-  }]);
+usBrowserControllers.controller('FacetInstancesListCtrl', [
+		'$scope',
+		'$http',
+		'$routeParams',
+		function($scope, $http, $routeParams) {
+			$http.get('/json-ld/facet/' + $routeParams.ruleform).success(
+					function(data) {
+						$scope.facets = data['@graph'];
+					});
+		} ]);
 
-usBrowserControllers.controller('FacetRuleformsListCtrl', ['$scope', '$http',
-  function ($scope, $http) {
-    $http.get('/json-ld/facet').success(function(data) {
-      $scope.facetRuleforms = data;
-    });
-  }]);
+usBrowserControllers.controller('FacetListCtrl', [
+		'$scope',
+		'$http',
+		'$routeParams',
+		function($scope, $http, $routeParams) {
+			$http.get('/json-ld/facet/' + $routeParams.ruleform).success(
+					function(data) {
+						$scope.facets = data['@graph'];
+					});
+		} ]);
 
-usBrowserControllers.controller('FacetDetailCtrl', ['$scope', '$http','$routeParams', 
-  function ($scope, $http, $routeParams) {
-    $http.get('/json-ld/facet/$routeParams.ruleform/$routeParams.instance').success(function(data) {
-      $scope.facet = data;
-    });
-  }]);
+usBrowserControllers.controller('FacetRuleformsListCtrl', [ '$scope', '$http',
+		function($scope, $http) {
+			$http.get('/json-ld/facet').success(function(data) {
+				$scope.facetRuleforms = data;
+			});
+		} ]);
+
+usBrowserControllers.controller('FacetDetailCtrl', [
+		'$scope',
+		'$http',
+		'$routeParams',
+		function($scope, $http, $routeParams) {
+			$http.get(
+					'/json-ld/facet/' + $routeParams.ruleform + '/'
+							+ $routeParams.classifier + '/'
+							+ $routeParams.classfication + '/'
+							+ $routeParams.instance).success(function(data) {
+				$scope.facet = data;
+			});
+		} ]);
+
+usBrowserControllers.controller('FacetInstanceDetailCtrl', [
+		'$scope',
+		'$http',
+		'$routeParams',
+		function($scope, $http, $routeParams) {
+			$http.get(
+					'/json-ld/facet/' + $routeParams.ruleform + '/'
+							+ $routeParams.instance).success(function(data) {
+				$scope.facet = data;
+			});
+		} ]);
--- phantasm-at-rest/src/test/resources/assets/js/ultra-browser.js
+++ phantasm-at-rest/src/test/resources/assets/js/ultra-browser.js
@@ -1,22 +1,21 @@
-var usBrowser = angular.module('usBrowser', [
-  'ngRoute',
-  'usBrowserControllers'
-]);
+var usBrowser = angular.module('usBrowser',
+		[ 'ngRoute', 'usBrowserControllers' ]);
 
-usBrowser.config(['$routeProvider',
-                    function($routeProvider) {
-                      $routeProvider.
-                        when('/json-ld/facet', {
-                          templateUrl: 'partials/facet-ruleforms.html',
-                          controller: 'FacetRuleformsListCtrl'
-                        }).
-                        when('/json-ld/facet/:ruleform', {
-                          templateUrl: 'partials/all-facet-instances.html',
-                          controller: 'FacetListCtrl'
-                        }).
-                        when('/json-ld/facet/:ruleform/:instance', {
-                          templateUrl: 'partials/facet-detail.html',
-                          controller: 'FacetDetailCtrl'
-                        })
-                        $routeProvider.otherwise({redirectTo: '/json-ld/facet'});
-                    }]);
+usBrowser.config([ '$routeProvider', function($routeProvider) {
+	$routeProvider.when('/json-ld/facet', {
+		templateUrl : 'partials/facet-ruleforms.html',
+		controller : 'FacetRuleformsListCtrl'
+	}).when('/json-ld/facet/:ruleform', {
+		templateUrl : 'partials/all-facets.html',
+		controller : 'FacetListCtrl'
+	}).when('/json-ld/facet/:ruleform/:classifier/:classification', {
+		templateUrl : 'partials/all-facet-instances.html',
+		controller : 'FacetDetailCtrl'
+	}).when('/json-ld/facet/:ruleform/:classifier/:classification/:instance', {
+		templateUrl : 'partials/facet-instance-detail.html',
+		controller : 'FacetInstanceDetailCtrl'
+	})
+	$routeProvider.otherwise({
+		redirectTo : '/json-ld/facet'
+	});
+} ]);

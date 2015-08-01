8ba9e28554541dbe5bb808cc066449e5919bdbfc
Hellblazer <hellblazer@me.com>
2015-07-31 18:12:22 -0700

    done by core module now

--- phantasm-at-rest/src/test/java/com/chiralbehaviors/CoRE/phantasm/jsonld/resources/test/TestApplication.java
+++ phantasm-at-rest/src/test/java/com/chiralbehaviors/CoRE/phantasm/jsonld/resources/test/TestApplication.java
@@ -30,8 +30,6 @@ import com.chiralbehaviors.CoRE.phantasm.jsonld.resources.RuleformResource;
 import com.chiralbehaviors.CoRE.phantasm.jsonld.resources.WorkspaceMediatedResource;
 import com.chiralbehaviors.CoRE.phantasm.jsonld.resources.WorkspaceResource;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
-import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module.Feature;
 
 import io.dropwizard.Application;
 import io.dropwizard.assets.AssetsBundle;
@@ -69,9 +67,6 @@ public class TestApplication extends Application<TestServiceConfiguration> {
                                              "browser"));
         ObjectMapper objMapper = bootstrap.getObjectMapper();
         objMapper.registerModule(new CoREModule());
-        Hibernate4Module module = new Hibernate4Module();
-        module.enable(Feature.FORCE_LAZY_LOADING);
-        objMapper.registerModule(module);
     }
 
     /* (non-Javadoc)

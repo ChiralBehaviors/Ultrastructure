a85fb570e6cd7411f302eced1e63570651e5ea65
Hellblazer <hellblazer@me.com>
2016-04-10 19:29:47 -0700

    moar coverage and trimming

--- phantasm-at-rest/src/main/java/com/chiralbehaviors/CoRE/phantasm/resources/TransactionalResource.java
+++ phantasm-at-rest/src/main/java/com/chiralbehaviors/CoRE/phantasm/resources/TransactionalResource.java
@@ -16,15 +16,11 @@
 
 package com.chiralbehaviors.CoRE.phantasm.resources;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.concurrent.Callable;
 import java.util.function.Function;
 
 import javax.ws.rs.WebApplicationException;
 
 import org.jooq.DSLContext;
-import org.jooq.exception.DataAccessException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -37,35 +33,6 @@ import com.chiralbehaviors.CoRE.security.AuthorizedPrincipal;
  */
 public class TransactionalResource {
     private final static Logger log = LoggerFactory.getLogger(TransactionalResource.class);
-
-    public static <T> T readOnly(Callable<T> call,
-                                 Model model) throws Exception {
-        Connection connection = model.create()
-                                     .configuration()
-                                     .connectionProvider()
-                                     .acquire();
-        connection.setReadOnly(true);
-        try {
-            return call.call();
-        } finally {
-            connection.setReadOnly(false);
-        }
-    }
-
-    @SuppressWarnings("unused")
-    private void setReadOnly(Model model, boolean value) {
-        try {
-            model.create()
-                 .configuration()
-                 .connectionProvider()
-                 .acquire()
-                 .setReadOnly(value);
-        } catch (DataAccessException e) {
-            throw new WebApplicationException(e, 500);
-        } catch (SQLException e) {
-            throw new WebApplicationException(e, 500);
-        }
-    }
 
     protected <T> T mutate(AuthorizedPrincipal principal,
                            Function<Model, T> txn,

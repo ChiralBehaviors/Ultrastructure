5c1d338da4f9c5a123d70cf8c8d5246566938ff1
Hellblazer <hellblazer@me.com>
2016-04-19 06:19:01 -0700

    populate facets list in the view!
    makin' progress

--- ide/src/main/java/com/chiralbehaviors/CoRE/occular/Occular.java
+++ ide/src/main/java/com/chiralbehaviors/CoRE/occular/Occular.java
@@ -1,6 +1,10 @@
 package com.chiralbehaviors.CoRE.occular;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import javax.ws.rs.client.ClientBuilder;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -11,17 +15,15 @@ import javafx.stage.Stage;
 
 public class Occular extends Application {
 
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     private Stage      primaryStage;
     private BorderPane rootLayout;
 
-    @Override
-    public void start(Stage primaryStage) {
-        this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("Workspace View");
-
-        initRootLayout();
-
-        showView();
+    public Stage getPrimaryStage() {
+        return primaryStage;
     }
 
     public void initRootLayout() {
@@ -38,23 +40,37 @@ public class Occular extends Application {
         }
     }
 
-    public void showView() {
+    public void showView(GraphQlApi api) {
         try {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(Occular.class.getResource("view/FacetsView.fxml"));
             AnchorPane workspaceView = (AnchorPane) loader.load();
 
             rootLayout.setCenter(workspaceView);
+            FacetsController facetsController = (FacetsController) loader.getController();
+            facetsController.setApi(api);
+            facetsController.update();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public Stage getPrimaryStage() {
-        return primaryStage;
-    }
-
-    public static void main(String[] args) {
-        launch(args);
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Workspace View");
+        String encoded;
+        try {
+            encoded = URLEncoder.encode("urn:uuid:00000000-0000-0004-0000-000000000003",
+                                        "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new IllegalStateException(e);
+        }
+        GraphQlApi api = new GraphQlApi(ClientBuilder.newClient()
+                                                     .target(String.format("http://localhost:5000/workspace/%s/meta",
+                                                                           encoded)),
+                                        null);
+        initRootLayout();
+        showView(api);
     }
 }

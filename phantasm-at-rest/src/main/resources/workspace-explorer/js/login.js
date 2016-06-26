344f75329cf1c0ef95a7e2f05c53b933563f11c9
Hallie Parry <halloran.parry@gmail.com>
2016-06-26 13:45:01 -0700

    Full interactive login/token flow with no server interaction

--- phantasm-at-rest/src/main/resources/workspace-explorer/js/login.js
+++ phantasm-at-rest/src/main/resources/workspace-explorer/js/login.js
@@ -1,36 +1,33 @@
-var LoginForm = React.createClass({
-    render: function() {
-        return (<div id="login_form" class="login-form">
-            <h2>Login Form</h2>
-            <label>User Name :</label> <input type="text" name="username"
-                                              id="username"></input>
-            <label>Password :</label> <input
-            type="password" name="password" id="password"> </input>
-            <input
-                type="button" name="login" id="login" value="Login"></input>
-        </div>
-        );
-    }
-});
-
-var LoginToken = React.createClass({
-    render: function() {
-        return (<div id="token">
-            <p>token goes here</p>
-            </div>
-        );
-    }
-})
-
 var Login = React.createClass({
+    getInitialState() {
+        return {
+            authToken: ""
+        }
+    },
+
+    getToken: function(e) {
+        e.preventDefault();
+        console.log("getting token ");
+        this.setState({authToken: "token"});
+    },
+
 	render: function() {
 		return (
             <div>
-                <LoginForm/>
-                <LoginToken/>
+                <form onSubmit={this.getToken} d="login_form" class="login-form">
+                    <h2>Login Form</h2>
+                    <label>User Name :</label> <input type="text" name="username"
+                                                      id="username"></input>
+                    <label>Password :</label> <input
+                    type="password" name="password" id="password"> </input>
+                    <button>Login</button>
+                </form>
+                <div id="token">
+                    <p>Token: {this.state.authToken}</p>
+                </div>
             </div>
 		);
 	}
 });
 
-        ReactDOM.render(<Login />, document.getElementById('login'));
+ReactDOM.render(<Login />, document.getElementById('login'));

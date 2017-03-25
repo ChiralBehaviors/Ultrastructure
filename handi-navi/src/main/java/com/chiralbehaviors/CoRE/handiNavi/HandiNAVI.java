a3ca1929cce748189779c952868810bacaef4a03
Hellblazer <hellblazer@me.com>
2017-03-24 17:08:22 -0700

    Use default when invoked with no args

--- handi-navi/src/main/java/com/chiralbehaviors/CoRE/handiNavi/HandiNAVI.java
+++ handi-navi/src/main/java/com/chiralbehaviors/CoRE/handiNavi/HandiNAVI.java
@@ -1,6 +1,11 @@
 package com.chiralbehaviors.CoRE.handiNavi;
 
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import com.chiralbehaviors.CoRE.phantasm.service.PhantasmApplication;
+import com.hellblazer.utils.Utils;
 
 import io.dropwizard.setup.Bootstrap;
 
@@ -12,7 +17,17 @@ import io.dropwizard.setup.Bootstrap;
  */
 public class HandiNAVI extends PhantasmApplication<EmbeddedConfiguration> {
 
+    private static final String DEFAULT_YML         = "/default.yml";
+    private static final String DEFAULT_YML_RUNTIME = ".default.yml";
+
     public static void main(String[] argv) throws Exception {
+        if (argv.length == 0) {
+            try (InputStream is = HandiNAVI.class.getResourceAsStream(DEFAULT_YML);
+                    OutputStream os = new FileOutputStream(DEFAULT_YML_RUNTIME)) {
+                Utils.copy(is, os);
+            }
+            argv = new String[] { "server", DEFAULT_YML_RUNTIME };
+        }
         new HandiNAVI().run(argv);
     }
 
